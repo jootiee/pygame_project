541f8886189f8a0d0403f6772a49d10c62667cf3
jootiee <72806024+jootiee@users.noreply.github.com>
2020-12-09 18:11:33 +0300

    hw

---
+++ config.py
@@ -0,0 +1,118 @@
+﻿import os
+
+TILE_SIZE = 64
+WIN_SIZE = (16 * TILE_SIZE, 9 * TILE_SIZE)
+FPS = 60
+
+# COLOR =     R    G    B
+WHITE = (255, 255, 255)
+BLACK = (0,   0,   0)
+BG_COLOR = (135, 206, 250)  # lightskyblue
+
+current_dir = os.path.dirname(__file__)
+assets = os.path.join(current_dir, 'assets')
+lvls = os.path.join(current_dir, 'lvls')
+
+CAT = os.path.join(assets, 'cat.png')
+PEPEGA = os.path.join(assets, 'pepega.png')
+RAT = os.path.join(assets, 'rat.png')
+SILVER_COIN = os.path.join(assets, 'silver.png')
+GOLD_COIN = os.path.join(assets, 'gold.png')
+SPIKE = os.path.join(assets, 'spike.png')
+MEDKIT = os.path.join(assets, 'medkit.png')
+
+blocks = os.path.join(assets, 'blocks')
+consumable = os.path.join(assets, 'consumables')
+char = os.path.join(assets, 'char')
+
+heal = os.path.join(consumable, 'heal')
+
+PLAYER_ASSETS = {'idle': os.path.join(char, 'character.png')}
+
+FOOD = [os.path.join(heal, 'Apple.png'),
+        os.path.join(heal, 'AppleWorm.png'),
+        os.path.join(heal, 'Avocado.png'),
+        os.path.join(heal, 'Bacon.png'),
+        os.path.join(heal, 'Beer.png'),
+        os.path.join(heal, 'Boar.png'),
+        os.path.join(heal, 'Bread.png'),
+        os.path.join(heal, 'Brownie.png'),
+        os.path.join(heal, 'Bug.png'),
+        os.path.join(heal, 'Cheese.png'),
+        os.path.join(heal, 'Cherry.png'),
+        os.path.join(heal, 'Chicken.png'),
+        os.path.join(heal, 'ChickenLeg.png'),
+        os.path.join(heal, 'Cookie.png'),
+        os.path.join(heal, 'DragonFruit.png'),
+        os.path.join(heal, 'Eggplant.png'),
+        os.path.join(heal, 'Eggs.png'),
+        os.path.join(heal, 'Fish.png'),
+        os.path.join(heal, 'FishFillet.png'),
+        os.path.join(heal, 'FishSteak.png'),
+        os.path.join(heal, 'Grub.png'),
+        os.path.join(heal, 'Grub.png'),
+        os.path.join(heal, 'Honey.png'),
+        os.path.join(heal, 'Honeycomb.png'),
+        os.path.join(heal, 'Jam.png'),
+        os.path.join(heal, 'Jerky.png'),
+        os.path.join(heal, 'Lemon.png'),
+        os.path.join(heal, 'Marmalade.png'),
+        os.path.join(heal, 'MelonCantaloupe.png'),
+        os.path.join(heal, 'MelonHoneydew.png'),
+        os.path.join(heal, 'MelonWater.png'),
+        os.path.join(heal, 'Moonshine.png'),
+        os.path.join(heal, 'Olive.png'),
+        os.path.join(heal, 'Onion.png'),
+        os.path.join(heal, 'PepperRed.png'),
+        os.path.join(heal, 'Pickle.png'),
+        os.path.join(heal, 'PickledEggs.png'),
+        os.path.join(heal, 'PieApple.png'),
+        os.path.join(heal, 'PieLemon.png'),
+        os.path.join(heal, 'PiePumpkin.png'),
+        os.path.join(heal, 'Pineapple.png'),
+        os.path.join(heal, 'Potato.png'),
+        os.path.join(heal, 'PotatoRed.png'),
+        os.path.join(heal, 'Pretzel.png'),
+        os.path.join(heal, 'Ribs.png'),
+        os.path.join(heal, 'Rol.png'),
+        os.path.join(heal, 'Saki.png'),
+        os.path.join(heal, 'PepperGreen.png'),
+        os.path.join(heal, 'Peach.png'),
+        os.path.join(heal, 'Pepperoni.png'),
+        os.path.join(heal, 'Sardines.png'),
+        os.path.join(heal, 'Sashimi.png'),
+        os.path.join(heal, 'Sausages.png'),
+        os.path.join(heal, 'Shrimp.png'),
+        os.path.join(heal, 'Steak.png'),
+        os.path.join(heal, 'Stein.png'),
+        os.path.join(heal, 'Strawberry.png'),
+        os.path.join(heal, 'Sushi.png'),
+        os.path.join(heal, 'Tart.png'),
+        os.path.join(heal, 'Tomato.png'),
+        os.path.join(heal, 'Turnip.png'),
+        os.path.join(heal, 'Waffles.png'),
+        os.path.join(heal, 'Whiskey.png'),
+        os.path.join(heal, 'Wine.png')]
+
+coins = os.path.join(consumable, 'coins')
+COINS = [os.path.join(coins, 'Coin_Blue.png'),
+         os.path.join(coins, 'Coin_Gold.png'),
+         os.path.join(coins, 'Coin_Green.png'),
+         os.path.join(coins, 'Coin_Purple.png'),
+         os.path.join(coins, 'Coin_Red.png')]
+
+LEVELS = [os.path.join(lvls, 'lvl1.txt')]
+
+
+BLOCK_ASSETS = {'ground': os.path.join(blocks, 'ground.png')}
+
+
+
+MAP = {
+    'C': COINS[0],
+    'B': BLOCK_ASSETS['ground'],
+    'S': SPIKE,
+    'F': FOOD[0],
+}
+
+SOLID_BLOCKS = 'BS'
---
+++ main.py
@@ -0,0 +1,273 @@
+import pygame
+import random
+import os
+import math
+
+# импорт констант + загрузка ассетов
+from config import *
+
+# родительский класс, от которого наследуются остальные
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, x=0, y=0, size=100, speed=0, image=PLAYER_ASSETS['idle']):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.speed = speed
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(x, y)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
+
+    def border_check(self):
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIN_SIZE[0]:
+            self.rect.right = WIN_SIZE[0]
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > WIN_SIZE[1]:
+            self.rect.bottom = WIN_SIZE[1]
+
+
+class Spike(Sprite):
+    def __init__(self, x=0, y=0, size=100, damage=25, speed=10, image=PLAYER_ASSETS['idle'][0]):
+        if x == None:
+            x = random.randint(0, WIN_SIZE[0] - size)
+        if y == None:
+            y = random.randint(0, WIN_SIZE[1] - size)
+        Sprite.__init__(self, x, y, size, speed, image)
+        self.damage = damage
+
+
+class Medkit(Sprite):
+    def __init__(self, x=None, y=None, size=50, speed=0, image=MEDKIT):
+        if x == None:
+            x = random.randint(0, WIN_SIZE[0] - size)
+        if y == None:
+            y = random.randint(0, WIN_SIZE[1] - size)
+        Sprite.__init__(self, x, y, size, speed, image)
+        self.value = 50
+
+
+class Coin(Sprite):
+    def __init__(self, x=None, y=None, value=5, size=50, speed=10, image=COINS[0]):
+        if x is None:
+            self.x = random.randint(0, WIN_SIZE[0] - size)
+        else:
+            self.x = x
+        if y is None:
+            self.y = random.randint(0, WIN_SIZE[1] - size)
+        else:
+            self.y = y
+        super().__init__(self.x, self.y, size, speed, image)
+        self.ticks = random.choice(range(0, 360, 5))
+        self.forward = 1
+        self.spawn = self.rect.topleft
+        self.value = value
+
+    def update(self, *args):
+        if self.ticks >= 360:
+            self.ticks -= 360
+        self.ticks += 5
+
+        if not self.speed:
+            return
+
+        dy = math.sin(math.radians(self.ticks)) * self.speed
+        self.rect.y = self.spawn[1] + dy
+
+
+class Player(Sprite):
+    spikes = None
+    coins = None
+    medkits = None
+    damage = None
+
+    def __init__(self, x=0, y=0, size=100, speed=10, image=PLAYER_ASSETS['idle']):
+        Sprite.__init__(self, x, y, size, speed, image)
+        self.money = 0
+        self.hp_max = 100
+        self.hp_start = self.hp_max
+        self.hp = self.hp_start
+        self.x = x
+        self.y = y
+        self.x0 = self.x
+        self.y0 = self.y
+        Sprite.__init__(self, self.x, self.y, size, speed, image)
+        self.image_main = self.image
+        self.image_flipped = pygame.transform.flip(self.image, True, False)
+        self.delay = 500
+        self.cooldown = self.delay
+
+    def take_damage(self, ms, damage):
+        if self.cooldown > 0:
+            self.cooldown -= ms
+            return 0
+        if self.cooldown <= 0:
+            self.cooldown = 0
+        if self.cooldown == 0:
+            self.cooldown = self.delay
+            return damage
+
+    def update(self, up, down, left, right, ms):
+        if left == right:
+            pass
+        elif left:
+            self.rect.x -= self.speed
+            self.image = self.image_flipped
+        else:
+            self.rect.x += self.speed
+            self.image = self.image_main
+        if up == down:
+            pass
+        elif up:
+            self.rect.y -= self.speed
+        else:
+            self.rect.y += self.speed
+
+        self.border_check()
+
+        if self.coins:
+            for coin in self.coins:
+                if pygame.sprite.collide_rect(self, coin):
+                    self.money += coin.value
+                    coin.kill()
+        else:
+            return True
+
+        if self.spikes:
+            for spike in self.spikes:
+                if pygame.sprite.collide_rect(self, spike):
+                    self.hp -= self.take_damage(ms, spike.damage)
+
+        if self.medkits:
+            for medkit in self.medkits:
+                if pygame.sprite.collide_rect(self, medkit):
+                    if self.hp + medkit.value < 100:
+                        self.hp += medkit.value
+                    else:
+                        self.hp = self.maxhp
+                    medkit.kill()
+
+        if self.hp <= 0:
+            self.respawn()
+
+    def respawn(self):
+        self.money = round(self.money * 0.9)
+        self.rect.topleft = (self.x0, self.y0)
+        self.hp = self.hp_max
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display = pygame.display.set_mode(WIN_SIZE)
+        pygame.display.set_caption('Вторые шаги котика')
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.down = self.up = self.left = self.right = False
+        self.objects = pygame.sprite.Group()
+        self.coins = pygame.sprite.Group()
+        self.spikes = pygame.sprite.Group()
+        self.medkits = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        Player.coins = self.coins
+        Player.spikes = self.spikes
+        Player.medkits = self.medkits
+        self.solid_blocks = pygame.sprite.Group()
+        self.load_map()
+        # for _ in range(random.randint(1, 25)):
+        #     Coin().add(self.coins, self.objects)
+        # for _ in range(random.randint(1, 15)):
+        #     Spike().add(self.spikes, self.objects)
+        # for _ in range(random.randint(1, 10)):
+        #     Medkit().add(self.medkits, self.objects)
+        spike = Spike(image=SPIKE)
+        self.player = Player()
+        self.player.add(self.objects)
+        self.played = 0.0
+        self.coins_collected = False
+        self.endgame_timecode = None
+        self.countdown = False
+
+    def load_map(self):
+        map_path = LEVELS[0]
+        with open(map_path, encoding='utf-8') as file:
+            for y, line in enumerate(file):
+                for x, letter in enumerate(line):
+                    if letter in MAP.keys():
+                        pos = x * TILE_SIZE, y * TILE_SIZE
+                        image = MAP[letter]
+                        if letter in SOLID_BLOCKS:
+                            block = Sprite(*pos, image=image)
+                            self.solid_blocks.add(block)
+                        if letter == 'S':
+                            block = Spike(*pos, image=image)
+                            self.spikes.add(block)
+                        elif letter == 'C':
+                            block = Coin(*pos, value=5, image=image)
+                            self.coins.add(block)
+                        self.objects.add(block)
+
+    def run(self):
+        while self.running:
+            self.events()
+            self.update()
+            self.render()
+
+    def events(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    self.down = True
+                if event.key == pygame.K_w:
+                    self.up = True
+                if event.key == pygame.K_a:
+                    self.left = True
+                if event.key == pygame.K_d:
+                    self.right = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_s:
+                    self.down = False
+                if event.key == pygame.K_w:
+                    self.up = False
+                if event.key == pygame.K_a:
+                    self.left = False
+                if event.key == pygame.K_d:
+                    self.right = False
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+
+    def update(self):
+        ms = self.clock.tick(FPS)
+        self.played += ms / 1000
+        pygame.display.set_caption(
+            f"Player's money: {self.player.money}. Player's hp: {self.player.hp} Played: {self.played:.2f}")
+        self.player.update(self.up, self.down, self.left, self.right, ms)
+        if self.player.update(self.up, self.down, self.left, self.right, ms) and not self.countdown:
+            self.countdown = 5.0
+        if self.countdown:
+            self.countdown -= ms / 1000
+            if self.countdown <= 0:
+                self.running = False
+        self.coins.update()
+
+    def render(self):
+        self.display.fill(BG_COLOR)
+        self.objects.draw(self.display)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+    pygame.quit()
